5a37682642f8100576dd0314b6a6ade1a709af1b
Your Name <your-email@example.com>
2025-12-07 17:17:48 +0200

    edit on index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import { sendEmailAndSave } from "../controllers/emailController.js";
+import { sendEmailAndSave } from "./controllers/emailController.js";
 
 dotenv.config();
 
